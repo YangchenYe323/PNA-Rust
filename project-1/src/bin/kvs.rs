849430931ad0de9837888be3d377e4953087033a
Ye Yangchen <yye02@email.wm.edu>
2022-04-09 16:57:49 -0400

    fix format

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -2,27 +2,22 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-	// operation to perform on database
-	op: Option<String>,
+    // operation to perform on database
+    op: Option<String>,
 
-	// key of operation
-	key: Option<String>,
+    // key of operation
+    key: Option<String>,
 
-	// value of operation
-	value: Option<String>,
-
+    // value of operation
+    value: Option<String>,
 }
 
 fn main() -> Result<(), String> {
-	let args = Args::parse();
+    let args = Args::parse();
 
-	match args.op {
-		Some(_) => {
-			Err(String::from("unimplemented"))
-		}
+    match args.op {
+        Some(_) => Err(String::from("unimplemented")),
 
-		None => {
-			Err(String::from("No Op provided"))
-		}
-	}
+        None => Err(String::from("No Op provided")),
+    }
 }
--- project-1/src/kvstore.rs
+++ project-1/src/kvstore.rs
@@ -2,40 +2,40 @@ use std::collections::BTreeMap;
 
 /// Data Structure handling the storage and retrieval
 /// of key-value data
-/// 
+///
 /// ```
 /// use kvs::KvStore;
-/// 
+///
 /// let mut store = KvStore::new();
 /// store.set(String::from("key"), String::from("value"));
 /// assert_eq!(Some(String::from("value")), store.get(String::from("key")));
+///
+#[derive(Default, Debug)]
 pub struct KvStore {
-	map: BTreeMap<String, String>,
+    map: BTreeMap<String, String>,
 }
 
 impl KvStore {
-	/// create a new KvStore instance
-	pub fn new() -> KvStore {
-		KvStore { 
-			map: BTreeMap::new(),
-		}
-	}
+    /// create a new KvStore instance
+    pub fn new() -> KvStore {
+        KvStore {
+            map: BTreeMap::new(),
+        }
+    }
 
-	/// set key-val pair in the store
-	pub fn set(&mut self, key: String, val: String) {
-		self.map.insert(key, val);
-	}
+    /// set key-val pair in the store
+    pub fn set(&mut self, key: String, val: String) {
+        self.map.insert(key, val);
+    }
 
-	/// get a copy of owned values associated with key
-	/// return None if no values is found
-	pub fn get(&self, key: String) -> Option<String> {
-		self.map.get(&key).map(|val| {
-			val.clone()
-		})
-	}
+    /// get a copy of owned values associated with key
+    /// return None if no values is found
+    pub fn get(&self, key: String) -> Option<String> {
+        self.map.get(&key).cloned()
+    }
 
-	/// remove key
-	pub fn remove(&mut self, key: String) {
-		self.map.remove(&key);
-	}
+    /// remove key
+    pub fn remove(&mut self, key: String) {
+        self.map.remove(&key);
+    }
 }
