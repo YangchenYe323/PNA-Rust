f61729eb16e73504d3bdd3a6c97d35f49a5f4366
Ye Yangchen <yye02@email.wm.edu>
2022-04-09 16:47:37 -0400

    make command line scaffolding work

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,4 +1,28 @@
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+	// operation to perform on database
+	op: Option<String>,
 
-fn main() {
-    println!("Hello, world!");
+	// key of operation
+	key: Option<String>,
+
+	// value of operation
+	value: Option<String>,
+
+}
+
+fn main() -> Result<(), String> {
+	let args = Args::parse();
+
+	match args.op {
+		Some(_) => {
+			Err(String::from("unimplemented"))
+		}
+
+		None => {
+			Err(String::from("No Op provided"))
+		}
+	}
 }
