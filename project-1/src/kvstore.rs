3c4935622e616011bfd8e0efa4b5b6b12a0e9b61
Ye Yangchen <yye02@email.wm.edu>
2022-04-09 16:48:06 -0400

    implement storage functions

--- project-1/src/kvstore.rs
+++ project-1/src/kvstore.rs
@@ -1,21 +1,27 @@
-pub struct KvStore {
+use std::collections::BTreeMap;
 
+pub struct KvStore {
+	map: BTreeMap<String, String>,
 }
 
 impl KvStore {
 	pub fn new() -> KvStore {
-		KvStore {  }
+		KvStore { 
+			map: BTreeMap::new(),
+		}
 	}
 
 	pub fn set(&mut self, key: String, val: String) {
-		unimplemented!()
+		self.map.insert(key, val);
 	}
 
 	pub fn get(&self, key: String) -> Option<String> {
-		unimplemented!()
+		self.map.get(&key).map(|val| {
+			val.clone()
+		})
 	}
 
 	pub fn remove(&mut self, key: String) {
-		unimplemented!()
+		self.map.remove(&key);
 	}
 }
