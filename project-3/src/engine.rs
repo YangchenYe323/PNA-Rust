ac8f91f139933532ad1810d030b5b86e9f5259b0
Ye Yangchen <yye02@email.wm.edu>
2022-04-13 20:43:18 -0400

    fix KvsEngine trait

--- project-3/src/engine.rs
+++ project-3/src/engine.rs
@@ -4,11 +4,11 @@ use crate::Result;
 /// of a key-value storage engine
 pub trait KvsEngine {
 	/// get the value of the given string key
-	fn get(key: String) -> Result<String>;
+	fn get(&mut self, key: String) -> Result<Option<String>>;
 
 	/// set the value of the string key
-	fn set(key: String, val: String) -> Result<()>;
+	fn set(&mut self, key: String, val: String) -> Result<()>;
 
 	/// remove the value of the key
-	fn remove(key: String) -> Result<()>;
+	fn remove(&mut self, key: String) -> Result<()>;
 }
