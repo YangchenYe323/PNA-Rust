bd713d18763b363a47caa13a85ac437ca546d651
Ye Yangchen <yye02@email.wm.edu>
2022-04-25 22:50:43 -0400

    refactor

--- project-2/src/kvstore.rs
+++ project-2/src/kvstore.rs
@@ -10,11 +10,9 @@ use std::path::{Path, PathBuf};
 // try to compact log under 2MB threshold
 const COMPACTION_THRESHOLD: u64 = 2 * 1024 * 1024;
 
-const LOGFILE: &'static str = "kv.log";
-
-/// Data Structure handling the storage and retrieval
-/// of key-value data
-///
+/// A Persistent Key-Value Storage that uses log-structure file
+/// under the hood.
+/// 
 /// ```
 /// use kvs_project_2::KvStore;
 /// use tempfile::TempDir;
@@ -46,7 +44,6 @@ pub struct KvStore {
     // safe because in-memory database does not refer to them and further write will
     // never be made to them.
     cur_gen: u64,
-
     // map each alive generation to a reader
     readers: BTreeMap<u64, PositionedBufReader<File>>,
     // we only write to the last generation, so one writer is sufficient
@@ -114,15 +111,16 @@ impl KvStore {
     /// set key-val pair in the store
     pub fn set(&mut self, key: String, val: String) -> Result<()> {
         let op = Ops::set(key, val);
-        // this is the position of this op in the log
+        // this is the position of the current op in the log
         let pos = self.writer.pos;
 
-        // write op to log
+        // write op to log, writer.pos is the end point of the current op
         serde_json::to_writer(&mut self.writer, &op)?;
         self.writer.flush()?;
 
         // update in-memory map between key and CommandPos
         if let Ops::Set { key, .. } = op {
+            // old_op is stale now
             if let Some(old_op) = self
                 .database
                 .insert(key, (self.cur_gen, pos, self.writer.pos - pos).into())
@@ -142,18 +140,23 @@ impl KvStore {
     /// return None if no values is found
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
         if let Some(cmd_pos) = self.database.get(&key) {
+
+            // read log entry of cmd_pos
             let reader = self
                 .readers
                 .get_mut(&cmd_pos.gen)
                 .expect("Cannot find log reader");
             reader.seek(SeekFrom::Start(cmd_pos.pos))?;
+
             let reader = reader.take(cmd_pos.len);
             let op: Ops = serde_json::from_reader(reader)?;
+
             if let Ops::Set { key: _, val } = op {
                 Ok(Some(val))
             } else {
                 Err(KVErrorKind::UnexpectedCommandType(key).into())
             }
+
         } else {
             Ok(None)
         }
@@ -171,16 +174,25 @@ impl KvStore {
             serde_json::to_writer(&mut self.writer, &op)?;
             self.writer.flush()?;
 
+            // handle compaction
+            if self.uncompacted > COMPACTION_THRESHOLD {
+                self.compact()?;
+            }
+
             Ok(())
         } else {
             Err(KVErrorKind::KeyNotFound(key).into())
         }
     }
 
+    // the procedure of compaction is:
+    // 1. copy existing key-value entries in the in-memory database to a new logfile
+    // 2. update in-memory database to point to new log entries
+    // 3. delete old log files
     fn compact(&mut self) -> Result<()> {
-        let compaction_gen = self.cur_gen + 1;
+        self.cur_gen += 1;
+        let compaction_gen = self.cur_gen;
         let mut compaction_writer = new_log_file(&self.dirpath, compaction_gen, &mut self.readers)?;
-        self.cur_gen += 2;
 
         // copy all the data stored in the in-memory database
         // to a new logfile, this ensures the new logfile contains
@@ -193,8 +205,8 @@ impl KvStore {
                 .get_mut(&cmd_pos.gen)
                 .expect("Cannot find log reader");
             reader.seek(SeekFrom::Start(cmd_pos.pos))?;
-            let mut reader = reader.take(cmd_pos.len);
 
+            let mut reader = reader.take(cmd_pos.len);
             let length = io::copy(&mut reader, &mut compaction_writer)?;
 
             // update in-memory database to relfect new log entry
@@ -203,7 +215,9 @@ impl KvStore {
         }
         compaction_writer.flush()?;
 
-        // delete current log files
+        // delete current log files, up to this point
+        // these logfiles are replicated and can be safely deleted
+        // without risking losing data
         let gens_to_remove: Vec<u64> = self
             .readers
             .keys()
@@ -216,34 +230,58 @@ impl KvStore {
             fs::remove_file(logfile_path)?;
         }
 
-        self.writer = new_log_file(&self.dirpath, self.cur_gen, &mut self.readers)?;
+        self.writer = compaction_writer;
         self.uncompacted = 0;
 
         Ok(())
     }
 }
 
+// capture all the logfiles in the given directory
+// of form "<num>.log"
 fn sorted_gen_list(path: &Path) -> Result<Vec<u64>> {
-    let mut gen_list: Vec<u64> = fs::read_dir(path)?
-        .flat_map(|res| -> Result<_> { Ok(res?.path()) })
-        .filter(|path| path.is_file() && path.extension() == Some("log".as_ref()))
-        .flat_map(|path| {
-            path.file_name()
-                .and_then(OsStr::to_str)
-                .map(|s| s.trim_end_matches(".log"))
-                .map(str::parse::<u64>)
-        })
-        .flatten()
-        .collect();
+    let mut gens = vec![];
 
-    gen_list.sort_unstable();
-    Ok(gen_list)
+    let files = fs::read_dir(path)?;
+
+    for entry in files.into_iter() {
+        let filename = entry?.path();
+        // skip directories and files with other extension
+        if filename.is_file() && filename.extension() == Some("log".as_ref()) {
+            // println!("{:?}", filename);
+            // parse out the generation number, note that parsing error
+            // are tolerated and skipped, so "name.log" will not cause
+            // the program to crash
+            if let Some(name) = filename.file_name() {
+                if let Some(name_str) = OsStr::to_str(name) {
+                    // println!("{}", name_str);
+                    let gen = name_str.trim_end_matches(".log");
+                    if let Ok(gen) = gen.parse::<u64>() {
+                        // println!("{}", gen);
+                        gens.push(gen);
+                    }
+                }
+            }
+        }
+    }
+
+    gens.sort_unstable();
+
+    // println!("{:?}", gens);
+
+    Ok(gens)
 }
 
+// utility function to join directory path and a generation
+// number to a path to logfiles
 fn log_path(dirpath: &Path, gen: u64) -> PathBuf {
     dirpath.join(format!("{}.log", gen))
 }
 
+// read from logfile "<dir>/<gen>.log"
+// and update entries in database according to new logs
+// book-keep the reader for this file for future use
+// returns the number of stale entries in this file
 fn load_from_logfile(
     gen: u64,
     reader: &mut PositionedBufReader<File>,
@@ -275,6 +313,9 @@ fn load_from_logfile(
     Ok(uncompacted)
 }
 
+// create a new logfile "<dirpath>/gen.log"
+// book-keep the readers to logfiles
+// and return a writer for the new logfile
 fn new_log_file(
     dirpath: &Path,
     gen: u64,
@@ -293,6 +334,7 @@ fn new_log_file(
     Ok(writer)
 }
 
+// Wrapper around BufReader that is aware of its current offset
 #[derive(Debug)]
 struct PositionedBufReader<R: Read + Seek> {
     reader: BufReader<R>,
@@ -325,7 +367,7 @@ impl<R: Read + Seek> Seek for PositionedBufReader<R> {
         Ok(new_pos)
     }
 }
-
+// Wrapper around BufWriter that is aware of its current offset 
 #[derive(Debug)]
 struct PositionedBufWriter<W: Write + Seek> {
     writer: BufWriter<W>,
@@ -361,6 +403,9 @@ impl<W: Write + Seek> Seek for PositionedBufWriter<W> {
     }
 }
 
+// Describes a log on disk
+// stored in file "<dirpath>/<gen>.log",
+// with offset pos and length len
 #[derive(Debug, Copy, Clone)]
 struct CommandPos {
     gen: u64,
